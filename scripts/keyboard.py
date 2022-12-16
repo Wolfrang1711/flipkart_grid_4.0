04ae020f4acff735f5cab8f7f12422de455fe22d
Wolfrang1711 <121id0437@nitrkl.ac.in>
2022-12-16 13:47:41 +0530

    Work till now

---
+++ scripts/keyboard.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+© Copyright 2015-2016, 3D Robotics.
+simple_goto.py: GUIDED mode "simple goto" example (Copter Only)
+
+Demonstrates how to arm and takeoff in Copter and how to navigate to points using Vehicle.simple_goto.
+
+Full documentation is provided at http://python.dronekit.io/examples/simple_goto.html
+"""
+
+from __future__ import print_function
+import time
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+from pymavlink import mavutil
+
+# Set up option parsing to get connection string
+import argparse
+def connectMyCopter():
+    parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+    parser.add_argument('--connect')
+    args = parser.parse_args()
+
+    connection_string = args.connect
+    sitl = None
+
+
+    # Start SITL if no connection string specified
+    if not connection_string:
+        import dronekit_sitl
+        sitl = dronekit_sitl.start_default()
+        connection_string = sitl.connection_string()
+
+
+    # Connect to the Vehicle
+    print('Connecting to vehicle on: %s' % connection_string)
+    vehicle = connect(connection_string, wait_ready=True)
+    return vehicle
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+   
+    vehicle.armed = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto
+    #  (otherwise the command after Vehicle.simple_takeoff will execute
+    #   immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
+    """
+    Move vehicle in direction based on specified velocity vectors.
+    """
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, 0, 0, # x, y, z positions (not used)
+        velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
+        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+
+
+    # send command to vehicle on 1 Hz cycle
+    for x in range(0,duration):
+        vehicle.send_mavlink(msg)
+        time.sleep(1)
+vehicle = connectMyCopter()
+vehicle.mode= VehicleMode("GUIDED")
+arm_and_takeoff(10)
+
+print("Set default/target airspeed to 3")
+vehicle.airspeed = 3
+
+print("Going towards first point for 30 seconds ...")
+point1 = LocationGlobalRelative(-35.363263, 149.165233, 10)
+vehicle.simple_goto(point1)
+
+# sleep so we can see the change in map
+time.sleep(30)
+
+print("Going towards second point for 30 seconds (groundspeed set to 10 m/s) ...")
+point2 = LocationGlobalRelative(-35.363269, 149.165233, 10)
+vehicle.simple_goto(point2, groundspeed=10)
+
+# sleep so we can see the change in map
+time.sleep(30)
+
+#print("Returning to Launch")
+#vehicle.mode = VehicleMode("RTL")
+
+# Close vehicle object before exiting script
+#print("Close vehicle object")
+vehicle.mode = VehicleMode("LAND")
+time.sleep(2)
+while True:
+    time.sleep(2)
+vehicle.close()
+
+# Shut down simulator if it was started.
+#if sitl:
+   # sitl.stop()
